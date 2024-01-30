ffc6f012cb4c06181dfb48b35c416c51b96c7d79
Alan Khismatullin <alanhis043@gmail.com>
2024-01-30 13:38:41 +0500

    Основа проекта
     (с первичными функциями)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-color';
+  response: any;
+  input = ["N", "N", "N", "N", "N"];
+  constructor(private http: HttpClient) { }
+  searchColor() {
+    this.http.post('http://colormind.io/api/', JSON.stringify({ "input": this.input, "model": "default" })).subscribe((data: any) => {
+      this.response = data.result
+
+    })
+  }
+  lockColor(index: number) {
+    if (this.input[index] === "N") {
+      this.input[index] = this.response[index];
+    } else {
+      this.input[index] = "N";
+    }
+
+  }
+  saveColor(index: number) {
+    const color = this.response[index];
+    navigator.clipboard.writeText("#" + ((1 << 24) + (color[0] << 16) + (color[1] << 8) + color[2]).toString(16).slice(1));
+  }
+  ngOnInit() {
+    this.searchColor()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
+import { NgModule, importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [importProvidersFrom(HttpClientModule),],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
